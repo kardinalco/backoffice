1936bac25ec24c7ea2b5c8b76d8f634ac47f4201
Nicolas SANS <nicolas.sansd@gmail.com>
2024-12-17 09:55:03 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <SidebarProvider>
+          <AppSidebar />
+          <main>
+              <SidebarTrigger />
+              {children}
+          </main>
+      </SidebarProvider>
       </body>
     </html>
   );
